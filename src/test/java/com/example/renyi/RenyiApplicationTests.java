d65ea1d4d3400c9391462ccbd17b4b3309bdbbf8
xiejiujiang <634482891@qq.com>
2022-02-11 14:04:10 +0800

    测试提交2444

--- src/test/java/com/example/renyi/RenyiApplicationTests.java
+++ src/test/java/com/example/renyi/RenyiApplicationTests.java
@@ -27,45 +27,6 @@ import java.util.List;
 class RenyiApplicationTests {
 
     @Test
-    void contextLoads() {
-        try {
-            //创建client
-            DefaultChanjetClient defaultChanjetClient = new DefaultChanjetClient("https://openapi.chanjet.com");
-
-            //创建请求对象
-            CreateTenantRequest createTenantRequest = new CreateTenantRequest();
-
-            //设置开放平台公共请求参数
-            createTenantRequest.setAppKey("zMqBZ...");
-            createTenantRequest.setAppSecret("5DB7FB61... ...");
-            createTenantRequest.setRequestUri("/financial/orgAndUser/createTenant");
-
-            //设置header参数,接口如无appKey、appSecret、openToken、Content-Type四个参数之外的请求头，则不需要传
-            createTenantRequest.addHeader("key", "value");
-
-            //设置query参数,接口无query参数则不需要传
-            createTenantRequest.addQueryParam("key", "value");
-
-            //设置业务参数对象
-            CreateTenantContent createTenantContent = new CreateTenantContent();
-            createTenantContent.setTenantId("tenant_987... ...");
-            createTenantRequest.setBizContent(createTenantContent);
-            //发起请求并响应
-            CreateTenantResponse createTenantResponse = defaultChanjetClient.execute(createTenantRequest);
-            if (createTenantResponse.getResult()) {
-                System.out.println("调用成功。");
-            } else {
-                System.out.println("调用失败，原因：" + createTenantResponse.getError().getMsg());
-            }
-        } catch (ChanjetApiException e) {
-            //做异常处理
-            System.out.println("调用遭遇异常，原因：" + e.getMessage());
-            throw new RuntimeException(e.getMessage(), e);
-        }
-    }
-
-
-    @Test
     void gg(){
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
